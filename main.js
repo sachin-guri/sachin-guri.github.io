4cb7e8db9b5bf85adb0950775ca8cc1c1d796aab
sachin-guri <113023915+sachin-guri@users.noreply.github.com>
2026-01-14 15:13:39 +0000

    Deploying to gh-pages from @ sachin-guri/sachin-guri.github.io@41537ed59727cfeb2a5734341a6a4dff5cd05dcf 🚀

--- main.js
+++ main.js
@@ -15033,6 +15033,13 @@ function withPreloading(preloadingStrategy) {
   }];
   return routerFeature(0, providers);
 }
+function withHashLocation() {
+  const providers = [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }];
+  return routerFeature(6, providers);
+}
 function withComponentInputBinding() {
   const providers = [RoutedComponentInputBinder, {
     provide: INPUT_BINDER,
@@ -33074,7 +33081,7 @@ function provideAnimationsAsync(type = "animations") {
 var appConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimationsAsync(),
     provideHttpClient()
   ]
